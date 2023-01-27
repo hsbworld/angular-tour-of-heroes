4b350bad043e733978415b6771decd913d6ecf04
Haseeb <haseeb@ubuntu-nodejs.fedora>
2023-01-27 20:05:45 +0530

    Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,17 +11,38 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
 
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero : Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
-  heroes = HEROES;
+  heroes : Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService){
+
+  }
+
+  // getHeroes(){
+  //   this.heroes = this.heroService.getHeroes();
+  // }    
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  // heroes = HEROES;
 
   selectedHero ?: Hero;
 
   onSelectHero(hero: Hero) {
     this.selectedHero = hero;
+    // this.messageService.add(`Hero ${hero.name} clicked`);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
